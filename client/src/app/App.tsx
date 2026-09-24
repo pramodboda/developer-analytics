d269a3022db95a3d8bf4b819b75f92a3af418e8c
pramodboda <pramodkumarboda@gmail.com>
2026-09-24 16:37:06 +0530

    UPDATE: Project Setup

---
+++ client/src/app/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+// import heroImg from '../assets/hero.png'
+// import reactLogo from '../assets/react.svg'
+// import viteLogo from '../assets/vite.svg'
+import './App.css'
+
+import DeveloperLayout from "../layouts/DashboardLayout/DashboardLayout"
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <DeveloperLayout/>
+    </>
+  )
+}
+
+export default App
