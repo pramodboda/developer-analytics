20b02d800da7f55661e8be00cf9813f4ed8e9bdb
pramodboda <pramodkumarboda@gmail.com>
2026-09-24 21:35:55 +0530

    UPDATE: Project Setup

--- client/src/app/router/AppRouter.tsx
+++ client/src/app/router/AppRouter.tsx
@@ -20,7 +20,7 @@ const router = createBrowserRouter([
             },
             {
                 path:"proui",
-                // element:<></>,
+                // element:<ProUILayout />,
                 children: [
                   
                     {
