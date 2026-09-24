9672fa0d5be0fa5148dcac67839fe177172a8300
pramodboda <pramodkumarboda@gmail.com>
2026-09-24 21:34:39 +0530

    UPDATE: Project Setup

--- client/src/app/data/navItems.ts
+++ client/src/app/data/navItems.ts
@@ -1,42 +1,42 @@
 interface NavItem {
-    linkName: string;
-    link?: string;
-    children?: NavItem[];
-  }
-  
-  export const navItems: NavItem[] = [
-    {
-      linkName: "Home",
-      link: "/",
-    },
-    {
-      linkName: "Pro UI",
-      children: [
-        {
-          linkName: "UI Guidelines",
-          link: "/proui/ui-guidelines",
-        },
-        {
-            linkName: "Colors",
-            children: [
-              {
-                linkName: "Brand Colors",
-                link: "/proui/colors/brand",
-              },
-              {
-                linkName: "Semantic Colors",
-                link: "/proui/colors/semantic",
-              },
-            ],
+  linkName: string;
+  link?: string;
+  children?: NavItem[];
+}
+
+export const navItems: NavItem[] = [
+  {
+    linkName: "Home",
+    link: "/",
+  },
+  {
+    linkName: "Pro UI",
+    children: [
+      {
+        linkName: "UI Guidelines",
+        link: "/proui/ui-guidelines",
+      },
+      {
+        linkName: "Colors",
+        children: [
+          {
+            linkName: "Brand Colors",
+            link: "/proui/colors/brand",
           },
           {
-            linkName: "Typography",
-            link: "/ui-guidelines/typography",
+            linkName: "Semantic Colors",
+            link: "/proui/colors/semantic",
           },
-        {
-          linkName: "Components",
-          link: "/ui-guidelines/components",
-        },
-      ],
-    },
-  ];
+        ],
+      },
+      {
+        linkName: "Typography",
+        link: "/ui-guidelines/typography",
+      },
+      {
+        linkName: "Components",
+        link: "/ui-guidelines/components",
+      },
+    ],
+  },
+];
