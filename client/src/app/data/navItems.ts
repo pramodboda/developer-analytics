b651697cfad74b8be3182ad6b28788ce955b0ac9
pramodboda <pramodkumarboda@gmail.com>
2026-09-24 20:53:37 +0530

    UPDATE: Project Setup

--- client/src/app/data/navItems.ts
+++ client/src/app/data/navItems.ts
@@ -14,7 +14,7 @@ interface NavItem {
       children: [
         {
           linkName: "UI Guidelines",
-          link: "/proui/ui-guidelines",
+          link: "ui-guidelines",
         },
         {
             linkName: "Colors",
